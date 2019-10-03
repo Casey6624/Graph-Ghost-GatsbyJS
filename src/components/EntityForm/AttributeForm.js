82237a5317f9ec576aba4240bb23f0d33ed7e81c
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-10-03 17:42:35 +0100

    Wired up onChange handler and onSubmit handler so we now store the AttributeForm data inside of state. Next need to add validation

--- src/components/EntityForm/AttributeForm.js
+++ src/components/EntityForm/AttributeForm.js
@@ -4,9 +4,32 @@ import './AttributeForm.css'
 
 export default function({ value, onChange }) {
   const [status, setStatus] = useState(false)
+  const [attributeName, setAttributeName] = useState('')
+  const [attributeDataType, setAttributeDataType] = useState('')
 
+  // Used to display the Add New Attribute Form
   function handleClickEvent(e) {
     setStatus(!status)
+  }
+
+  // handle the dynamic change of form elements
+
+  function handleFormChange({ target }) {
+    // destructure the values and the names
+    const { name, value } = target
+    console.log(`Name: ${name} Value: ${value}`)
+    if (name === 'attributeName') {
+      setAttributeName(value)
+    } else if (name === 'attributeDataType') {
+      setAttributeDataType(value)
+    }
+    return
+  }
+
+  // Store form elements in state
+
+  function handleSubmit(e) {
+    console.log(attributeName, attributeDataType)
   }
 
   if (!status) {
@@ -20,27 +43,41 @@ export default function({ value, onChange }) {
   return (
     <Fragment>
       <div className="attributeContainer">
-        <label htmlFor="Attribute1">
+        <label htmlFor="AttributeName">
           Attribute 1
           <input
             type="text"
-            name="Attribute1"
+            onChange={e => handleFormChange(e)}
+            name="attributeName"
             placeholder="Attribute 1 Name"
             className="inline-form-item"
           ></input>
-          <select className="inline-form-item">
+          <select
+            className="inline-form-item"
+            name="attributeDataType"
+            onChange={e => handleFormChange(e)}
+          >
             <option defaultChecked disabled required>
               Select Data Type
             </option>
             <option name="string">String</option>
             <option name="int">Int</option>
-            <option name="date">Date (Saved as ISOString)</option>
+            <option name="date">Date</option>
             <option name="bool">Bool</option>
           </select>
-          <button className="button">
-            Save Attribute
-            <span className="icon style2 fa-save" id="EntityClose"></span>{' '}
-          </button>
+          <br />
+          <div
+            className="button"
+            style={{
+              alignItems: 'center',
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+            onClick={e => handleSubmit(e)}
+          >
+            Save Attribute{' '}
+            <span className="icon style2 fa-save" id="EntityClose"></span>
+          </div>
         </label>
       </div>
     </Fragment>
