5bc6f8ae82886ea4ace196110646e7633583f495
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-10-23 19:33:21 +0100

    Backup for today

--- src/pages/code.js
+++ src/pages/code.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
@@ -6,13 +6,49 @@ import HeaderCode from '../components/HeaderCode'
 import pic04 from '../assets/images/pic04.jpg'
 
 export default function Code(props) {
+  const GRAPHQL_URL = 'http://localhost:4500/graphql'
+
+  const [codeId, setCodeId] = useState(null)
+  const [creatorId, setCreatorId] = useState(null)
+  const [fetchedData, setFetchedData] = useState(null)
+
   useEffect(() => {
     let rawParams = props.location.search
-    let codeId = rawParams.split('=')[1].split('&')[0]
-    let creatorId = rawParams.split('=')[2]
-    console.log('codeId: ', codeId)
-    console.log('creatorId: ', creatorId)
+    setCodeId(rawParams.split('=')[1].split('&')[0])
+    setCreatorId(rawParams.split('=')[2])
   }, [props])
+
+  useEffect(() => {
+    if (fetchedData) return
+
+    let requestBody = `query{
+      findCodeRedirect(creatorId: "${creatorId}", codeId: "${codeId}"){
+        _id
+        generatedCode
+        retrievalCode
+        createdAt
+        updatedAt
+      }
+    }`
+
+    fetch(GRAPHQL_URL, {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed to fetch data!')
+        }
+        return res.json()
+      })
+      .then(data => {
+        console.log('data: ', data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [codeId, creatorId])
 
   return (
     <Layout>
@@ -23,44 +59,7 @@ export default function Code(props) {
           <span className="image main">
             <img src={pic04} alt="" />
           </span>
-          <h2>Magna feugiat lorem</h2>
-          <p>
-            Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus. Pellentesque venenatis dolor imperdiet dolor
-            mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor
-            sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet,
-            fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit.
-            Donec urna ex, lacinia in purus ac, pretium pulvinar mauris.
-            Curabitur sapien risus, commodo eget turpis at, elementum convallis
-            fames ac ante ipsum primis in faucibus.
-          </p>
-          <p>
-            Pellentesque venenatis dolor imperdiet dolor mattis sagittis.
-            Praesent rutrum sem diam, vitae egestas enim auctor sit amet.
-            Consequat leo mauris, consectetur id ipsum sit amet, fersapien
-            risus, commodo eget turpis at, elementum convallis elit enim turpis
-            lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id
-            ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat
-            malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna
-            est aliquam sem, sit amet est velit quis lorem.
-          </p>
-          <h2>Tempus veroeros</h2>
-          <p>
-            Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus
-            nunc placerat. Tempus adipiscing turpis non blandit accumsan eget
-            lacinia nunc integer interdum amet aliquam ut orci non col ut ut
-            praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac
-            commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante
-            accumsan nisi eu placerat gravida placerat adipiscing in risus fusce
-            vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet
-            lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing
-            accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a
-            iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc
-            lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur
-            ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing
-            nulla. Non vestibulum ac interdum condimentum semper commodo massa
-            arcu.
-          </p>
+          <h2>Here is your code!</h2>
         </section>
       </div>
     </Layout>
