1a64e14f2e3f5ee7d37e16eaea48d096f9b98503
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-11-14 21:09:44 +0000

    More work on grabbing state items ready to parse within prettier

--- src/pages/code.js
+++ src/pages/code.js
@@ -7,13 +7,19 @@ import pic04 from '../assets/images/pic04.jpg'
 
 export default function Code(props) {
   const GRAPHQL_ENDPOINT = 'http://localhost:4500/graphql'
-
+  // values decoded from the URL
   const [codeId, setCodeId] = useState(null)
   const [creatorId, setCreatorId] = useState(null)
+  // creatorID from server
+  const [serverCreatorID, setServerCreatorID] = useState(null)
+  // retrieval code from the server
+  const [retrieval, setRetrievalCode] = useState(null)
+  // Parsed code from the API
   const [rawCodeEntities, setRawCodeEntities] = useState(null)
+  // Error array which catches any issues with the pulled data from the server
   const [errors, setErrors] = useState([])
 
-  // Runs before painting the UI
+  // Runs before painting the UI, redirect if no creatorID or codeID
   useLayoutEffect(() => {
     let rawParams = props.location.search
     if (!rawParams) {
@@ -29,11 +35,14 @@ export default function Code(props) {
   }, [props])
 
   useEffect(() => {
-    console.log(rawCodeEntities)
+    console.log('rawCode', rawCodeEntities)
+    console.log('retrievalCode', retrieval)
+    console.log('creatorID From Server', serverCreatorID)
+    console.log('errors: Could be empty', errors)
   }, [rawCodeEntities])
 
   useEffect(() => {
-    if (!codeId || !creatorId) return
+    if (!codeId || !creatorId || rawCodeEntities || retrieval) return
 
     const requestBody = {
       query: `query{
@@ -61,10 +70,35 @@ export default function Code(props) {
         return res.json()
       })
       .then(({ data }) => {
-        const { generatedCode, retrievalCode, creator } = data.findCodeRedirect
+        const {
+          generatedCode,
+          retrievalCode: serverRetrievalCode,
+          creator,
+        } = data.findCodeRedirect
         if (!generatedCode) {
+          const tempErrors = [...errors]
+          tempErrors.push('No matching code could be pulled from the server!')
+          setErrors(tempErrors)
         }
+        if (!serverRetrievalCode) {
+          const tempErrors = [...errors]
+          tempErrors.push(
+            'No matching retrieval code could be found by the server'
+          )
+          setErrors(tempErrors)
+        }
+        //TODO - ensure this is not firing by mistake. Maybe remove the creatorID alltogether as we are getting this from the URL anyway. CHECK RETRIEVAL IS SPELT RIGHT
+        if (!creator) {
+          const tempErrors = [...errors]
+          tempErrors.push(
+            'There was an issue with your creator account. Please try again later'
+          )
+          setErrors(tempErrors)
+        }
+        if (errors.length > 0) return // bail out of setting variables as they are undefined
         setRawCodeEntities(JSON.parse(generatedCode))
+        setRetrievalCode(serverRetrievalCode)
+        setServerCreatorID(creator)
       })
       .catch(err => {
         console.log(err)
