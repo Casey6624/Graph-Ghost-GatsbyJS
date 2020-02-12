671084a03f6a9c08d0365df491b0a5e3c99ca1a5
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2020-02-12 17:08:29 +0000

    Update the Site Meta Data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Stellar by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Stellar by HTML5 UP"
+    title: 'Graph Ghost!',
+    author: 'Casey Smith',
+    description: 'Generate GraphQL Schema and Resolvers on the fly, instantly.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -19,6 +19,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
   ],
 }
